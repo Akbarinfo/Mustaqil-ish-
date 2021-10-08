1c8a01abbad5197836fe7abb0e62339080d69e07
Akbarinfo <girtbezbet@gmail.com>
2021-10-09 01:39:14 +0500

    Class metodlar yozildi

---
+++ login.py
@@ -0,0 +1,37 @@
+class Register:
+    def __init__(self):
+        pass
+
+### Bosh menyu
+    def menyu(self):
+        pass
+
+### Ro'yhatdan o'tish
+    def reg(self):
+        pass
+
+### Kirish
+    def kirish(self):
+        pass
+
+### shaxsiy bolim
+    def shaxsiy(self):
+        pass
+
+### Login o'zgartirish
+    def ologin(self):
+        pass
+
+### Parolni o'zgartirish
+    def oparol(self):
+        pass
+
+### Tizimdan chiqish
+    def logout(self):
+        pass
+
+### Accountni o'chirish
+    def delete(self):
+        pass
+
+window = Register()
